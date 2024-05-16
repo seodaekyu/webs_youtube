c6fadda5960c49ae51be60b718267eb67be2974d
seodaekyu <grarlove7@naver.com>
2024-05-16 17:48:02 +0900

    메인 컨텐츠 작업

--- src/components/section/Footer.jsx
+++ src/components/section/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer id='footer' role='contentinfo'>
-        Footer
+        <a href="mailto:grarlove7@naver.com" rel='noopenner noreferrer'>
+           grarlove7@naver.com
+        </a>
     </footer>
   )
 }
