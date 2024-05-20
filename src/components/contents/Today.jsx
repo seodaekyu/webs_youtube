b511cda7cbd147400cb05aededb410dd0a271f3f
seodaekyu <grarlove7@naver.com>
2024-05-20 18:15:25 +0900

    Swiper 이미지 슬라이드 작업

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { todayText } from '../../data/today'
 import { Link } from 'react-router-dom'
 
 const Today = () => {
-  return (
-    <section id='today'>
-      <div className='today__inner'>
-        <div className='today__thumb play__icon'>
-          <Link to={todayText[0].page}>
-              <img src={todayText[0].img} alt={todayText[0].title} />
-          </Link>
-        </div>
-        <div className='today__text'>
-              <span className='today'>today</span>
-              <h3 className='title'>
-                  <Link to={todayText[0].page}>
-                      {todayText[0].title}
-                  </Link>
-              </h3>
-              <p className='desc'>{todayText[0].desc}</p>
-              <div className='info'>
-                <span className='author'>
-                    <Link to={`/channel/${todayText[0].channelId}`}>
-                        {todayText[0].author}
+    const[loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 300);
+    }, []);
+
+    const todayClass = loading ? 'isLoading' : 'isLoaded'
+
+
+    return (
+        <section id='today' className={todayClass}>
+        <div className='today__inner'>
+            <div className='today__thumb play__icon'>
+            <Link to={todayText[0].page}>
+                <img src={todayText[0].img} alt={todayText[0].title} />
+            </Link>
+            </div>
+            <div className='today__text'>
+                <span className='today'>today</span>
+                <h3 className='title'>
+                    <Link to={todayText[0].page}>
+                        {todayText[0].title}
                     </Link>
-                </span>
-                <span className='date'>{todayText[0].date}</span>
-              </div>
+                </h3>
+                <p className='desc'>{todayText[0].desc}</p>
+                <div className='info'>
+                    <span className='author'>
+                        <Link to={`/channel/${todayText[0].channelId}`}>
+                            {todayText[0].author}
+                        </Link>
+                    </span>
+                    <span className='date'>{todayText[0].date}</span>
+                </div>
+            </div>
         </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Today
