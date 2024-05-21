dc17bca4e81e6d027aebb310204abd9c66ca7d65
seodaekyu <grarlove7@naver.com>
2024-05-21 17:22:59 +0900

    비디오 상세 페이지 만들기

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -16,7 +16,7 @@ const Search = () => {
     }, [searchId]);
 
     const fetchVideos = (query, pageToken = '') => {
-        fetchFromAPI(`search?part=snippet&q=${query}&pageToken=${pageToken}`)
+        fetchFromAPI(`search?part=snippet&type=video&q=${query}&pageToken=${pageToken}`)
             .then((data) => {
                 setNextPageToken(data.nextPageToken);
                 setVideos((prevVideos) => [...prevVideos, ...data.items]);
